587e96e7111017ad057d4c3284f893cd70f26bb9
geka001 <71638347+geka001@users.noreply.github.com>
2023-04-23 20:40:30 +0530

    Create unittest_prime.py

---
+++ Prime/UnitTests/unittest_prime.py
@@ -0,0 +1,8 @@
+import unittest
+from prime_number import is_prime
+class TestPrime(unittest.TestCase):
+	def test_prime_not_prime(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(9))
+        self.assertTrue(is_prime(11))
